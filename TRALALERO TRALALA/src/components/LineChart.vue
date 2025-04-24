10e4450fc6440c1d29adebfa940c7c1d86e46597
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-04-24 12:15:49 -0400

    got a chart with generic data to work

--- TRALALERO TRALALA/src/components/LineChart.vue
+++ TRALALERO TRALALA/src/components/LineChart.vue
@@ -8,15 +8,22 @@
 import { Line } from 'vue-chartjs'
 import {
   Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
-  Legend,
-  ArcElement,
-  CategoryScale,
-  RadialLinearScale,
+  Legend
 } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale, RadialLinearScale)
+ChartJS.register(  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend)
 
 const props = defineProps({
   data: Object,
--- TRALALERO TRALALA/src/views/MyProfile.vue
+++ TRALALERO TRALALA/src/views/MyProfile.vue
@@ -1,7 +1,24 @@
 <template>
-  <div></div>
+  <div><LineChart :data="data" :options="options"/></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LineChart from '@/components/LineChart.vue';
+
+const data = {  
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [40, 39, 10, 40, 39, 80, 40]
+    }
+  ]
+}
+
+const options = {  
+  responsive: true,
+  maintainAspectRatio: false}
+</script>
 
 <style scoped></style>
