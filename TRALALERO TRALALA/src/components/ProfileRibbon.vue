08ae474ddc8fed60f09269c7759f02c9c924b93e
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-04-24 12:32:17 -0400

    m

---
+++ TRALALERO TRALALA/src/components/ProfileRibbon.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1></h1>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import { useAuthStore } from '../stores/authStore'
+
+const auth = useAuthStore()
+
+</script>
+  
+<style scoped></style>
+  
--- TRALALERO TRALALA/src/views/MyProfile.vue
+++ TRALALERO TRALALA/src/views/MyProfile.vue
@@ -1,9 +1,14 @@
 <template>
-  <div><LineChart :data="data" :options="options"/></div>
+  <div v-if="auth.isLoggedIn"><LineChart :data="data" :options="options"/></div>
+  <div v-else><h2>YOU ARENT LOGGED IN</h2></div>
 </template>
 
 <script setup lang="ts">
 import LineChart from '@/components/LineChart.vue';
+import { useAuthStore } from '../stores/authStore'
+
+const auth = useAuthStore()
+
 
 const data = {  
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
