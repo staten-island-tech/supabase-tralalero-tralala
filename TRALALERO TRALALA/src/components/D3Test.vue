fc5326bb52eede6e29149218d80aa251da3f502c
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-05-02 12:29:36 -0400

    wserotfyurejhobrtniujnhtoip4jhnt4

---
+++ TRALALERO TRALALA/src/components/D3Test.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <D3LineChart :config="chart_config" :datum="chart_data"></D3LineChart>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+import { D3LineChart } from 'vue-d3-charts';
+
+  </script>
+  
