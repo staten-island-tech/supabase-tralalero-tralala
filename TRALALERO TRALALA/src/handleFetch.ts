134fcb6a92939931f3ee1bf64c55803629f42b7c
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-05-19 17:42:22 -0400

    m

--- TRALALERO TRALALA/src/handleFetch.ts
+++ TRALALERO TRALALA/src/handleFetch.ts
@@ -1,18 +1,19 @@
-const api_keys = import.meta.env.VITE_API_KEYS
 
-async function handleFetch(parameters : String) {
-    let t = true
-    let n = 0
+
+export async function handleFetch(parameters: string) {
+    const api_keys = import.meta.env.VITE_API_KEYS.split(',');
+
+    let t = true;
+    let n = 0;
 
     while (t && n < api_keys.length) {
-        try {
-            const response = await fetch(`https://www.alphavantage.co/query?${parameters}apikey=${api_keys[n]}`);
-            const rawData = await response.json(); // function=TIME_SERIES_INTRADAY&symbol=IBM&interval=15min&month=2009-01&outputsize=full&
-            return rawData
-        }
-        catch {
-            console.log("Next Key")
-            n++
-        }
+        
+        console.log("Key: " + n);
+        console.log("API Key: " + api_keys[n]);
+        const response = await fetch(`https://www.alphavantage.co/query?${parameters}&apikey=${api_keys[n]}`);
+        const rawData = await response.json();
+        console.log(rawData);
+        if (rawData["Information"]) n++;
+        else return rawData;
     }
 }
